988dfc4c90ce9bef66193e588517f636936acb4a
Oleh <boshaa3@gmail.com>
2019-10-30 21:10:51 +0200

    Updates

---
+++ precache-manifest.d6f1256fcef09c9af3f54dacb8001b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f975b5d9ec2615dfde12f7b26e6d05",
+    "url": "/develops-today-test/index.html"
+  },
+  {
+    "revision": "0c99607f7df9374e1ed3",
+    "url": "/develops-today-test/static/css/main.f164232d.chunk.css"
+  },
+  {
+    "revision": "14555556cdbc8f5b59e6",
+    "url": "/develops-today-test/static/js/2.6302022a.chunk.js"
+  },
+  {
+    "revision": "0c99607f7df9374e1ed3",
+    "url": "/develops-today-test/static/js/main.57632b5c.chunk.js"
+  },
+  {
+    "revision": "c1979c416747345f0a7a",
+    "url": "/develops-today-test/static/js/runtime-main.070cf329.js"
+  }
+]);
